23ea7c88dfd259bbae7f0af67e293c3c4c6126c2
ssusupernova <enojonovagrace@gmail.com>
2026-03-12 12:47:03 +0800

    new message text

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -2,6 +2,6 @@ export async function GET(request){
     const {searchParams} = new URL(request.url);
     const name = searchParams.get("name");
     return Response.json({
-        message: name ? `Hello, ${name}! What a wonderful day it it to die! You have successfully connected to the route.js` : "Successfully connected to the route.js"
+        message: name ? `Hello, ${name}! I hope you have a wonderful day today 😁! You have successfully connected to the route.js` : "Successfully connected to the route.js"
     });
 }
